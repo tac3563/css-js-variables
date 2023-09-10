4090acb671edddb3e57cc502c391b4569fb1ae2b
tac3563 <tcox_bu@outlook.com>
2023-09-10 16:31:07 +0100

    [Update] added blur slider to background image

--- CSS-JS-variables/src/main.ts
+++ CSS-JS-variables/src/main.ts
@@ -6,3 +6,12 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
   </div>
 `
+const blurSlider = document.getElementById('blur-slider');
+
+blurSlider?.addEventListener('change', inputHandler)
+blurSlider?.addEventListener('mousemove', inputHandler)
+
+function inputHandler(this: HTMLInputElement) {
+ const suffix = this.dataset.sizing; // dataset returns an object containing any custom data-* attributes on the selected element.
+ document.documentElement.style.setProperty(`--blur-filter`, this.value + suffix); // document.documentElement retrieves the root element (<html>) and setProperty() adds a property to the style object of the root element called --blur-filter and applies the value of this.value
+}
